93e48a8f9c27691fc84a9cb6bec329d539c44701
thibaultdvx <thibault.devarax@icm-institute.org>
2024-11-15 11:26:26 +0100

    flair generator

---
+++ tests/unittests/utils/caps_generator/test_flair_linear.py
@@ -0,0 +1,41 @@
+import shutil
+from pathlib import Path
+from clinicaio.utils.caps_generator.flair_linear import build_flair_linear
+
+
+def test_build_flair_linear(test_dir):
+    test_dir = Path(__file__).parents[2] / "tmp"
+    pipeline_dir = test_dir / "subjects" / "sub-002" / "ses-M003" / "flair_linear"
+
+    if pipeline_dir.exists():
+        shutil.rmtree(pipeline_dir)
+
+    build_flair_linear(root=test_dir, subject=2, session=3)
+    assert (
+        pipeline_dir
+        / "sub-002_ses-M003_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_FLAIR.nii.gz"
+    ).exists()
+    assert (
+        pipeline_dir / "sub-002_ses-M003_space-MNI152NLin2009cSym_res-1x1x1_affine.mat"
+    ).exists()
+    assert (
+        pipeline_dir
+        / "sub-002_ses-M003_space-MNI152NLin2009cSym_res-1x1x1_FLAIR.nii.gz"
+    ).exists()
+
+    shutil.rmtree(pipeline_dir)
+
+    build_flair_linear(root=test_dir, subject=2, session=3, crop=False)
+    assert not (
+        pipeline_dir
+        / "sub-002_ses-M003_space-MNI152NLin2009cSym_desc-Crop_res-1x1x1_FLAIR.nii.gz"
+    ).exists()
+    assert (
+        pipeline_dir / "sub-002_ses-M003_space-MNI152NLin2009cSym_res-1x1x1_affine.mat"
+    ).exists()
+    assert (
+        pipeline_dir
+        / "sub-002_ses-M003_space-MNI152NLin2009cSym_res-1x1x1_FLAIR.nii.gz"
+    ).exists()
+
+    shutil.rmtree(pipeline_dir)
