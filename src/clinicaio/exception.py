2add51cbc965b4e87520b3e254da30cdd0233f02
Jan-Michael Rye <jan-michael.rye@inria.fr>
2024-12-02 15:33:16 +0100

    Import source code from the BIDSPath project
    Import source code files from the BIDSPath project:
    https://gitlab.inria.fr/jrye/bidspath
    
    These files are the result of work initially started during a Hackathon
    at Inria's 2024 Digital Health Engineer seminar in Paris.

---
+++ src/clinicaio/exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Custom exceptions and exception handling."""
+
+
+class BIDSPathError(Exception):
+    """
+    Exception base class for custom exceptions raised by this package.
+    """
---
+++ src/clinicaio/subclasses/subject.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""BIDS subject class and functions."""
+
+from ..directory import BIDSDirectory
+
+
+class BIDSSubject(BIDSDirectory):
+    """
+    BIDS subject.
+    """
